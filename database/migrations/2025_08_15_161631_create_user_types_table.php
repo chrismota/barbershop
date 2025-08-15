49e2a6362262e630b39ed065b486de25bcca33d0
Christian Mota <christiancorreiamota@gmail.com>
2025-08-15 14:15:33 -0300

    refactor(migrations): rename type column in user_types table

--- database/migrations/2025_08_15_161631_create_user_types_table.php
+++ database/migrations/2025_08_15_161631_create_user_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_types', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('type');
+            $table->string('role');
             $table->timestamps();
             $table->softDeletes();
         });
