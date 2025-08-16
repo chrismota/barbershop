4d6d0d54de7aab735bccb69ca2c6a8129d65496c
Christian Mota <christiancorreiamota@gmail.com>
2025-08-16 20:40:40 -0300

    feat(models): add properties for uuid in Client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model {
+    public $incrementing = false;
+    protected $keyType = 'string';
+
     protected $fillable = [
         'phone',
         'address',
