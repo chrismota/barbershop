a891a4a7f3104c4be70eee99eccb1851ce6fcc39
Christian Mota <christiancorreiamota@gmail.com>
2025-08-16 20:38:25 -0300

    refactor(models): use $casts for password hashing in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Model {
         'password',
     ];
 
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
     public function userType(){
         return $this->belongsTo(UserType::class);
     }
@@ -31,22 +35,13 @@ class User extends Model {
         return $this->hasOne(Client::class);
     }
 
-     protected static function boot()
+    protected static function boot()
     {
         parent::boot();
 
         static::creating(function ($model) {
             if (!$model->id) {
                 $model->id = (string) Str::uuid();
-            }
-            if ($model->isDirty('password')) {
-                $model->password = bcrypt($model->password);
-            }
-        });
-
-        static::updating(function ($model) {
-            if ($model->isDirty('password')) {
-                $model->password = bcrypt($model->password);
             }
         });
     }
