9ab5aab5890d6789561644927f4bc9bacc1febd0
Christian Mota <christiancorreiamota@gmail.com>
2025-08-17 13:52:53 -0300

    feat(request): create UpdateClientRequest

---
+++ app/Http/Requests/UpdateClientRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Client;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateClientRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $clientId = $this->route('client');
+        $userId   = Client::find($clientId)?->user_id;
+
+        return [
+            'name'     => 'sometimes|string|min:5|max:255',
+            'email'    => "sometimes|email|unique:users,email,$userId",
+            'password' => 'sometimes|string|min:6',
+            'phone' => 'sometimes|string|min:9|max:11',
+            'address' => 'sometimes|string|min:5|max:255',
+            'city' => 'sometimes|string|min:3|max:255'
+        ];
+    }
+}
