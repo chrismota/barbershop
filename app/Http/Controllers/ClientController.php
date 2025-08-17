69089a0bd3cddabb4fc2e20e338eb61dc8d6b6d5
Christian Mota <christiancorreiamota@gmail.com>
2025-08-17 18:12:40 -0300

    refactor(controller): use ClientService in ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,93 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreClientRequest;
 use App\Http\Requests\UpdateClientRequest;
-use App\Models\Client;
-use App\Models\User;
-use App\Models\UserType;
+use App\Services\ClientService;
 
 class ClientController extends Controller
 {
-    public function index()
+    protected ClientService $clientService;
+
+    public function __construct(ClientService $clientService)
     {
-        return response()->json(Client::all());
+        $this->clientService = $clientService;
+    }
+
+    public function index(){
+        return response()->json($this->clientService->getAllClients(), 200);
     }
 
     public function show(string $id)
     {
-        $client = Client::find($id);
+        $client = $this->clientService->getClient($id);
 
-        if(!$client){
-            return response()->json(['message' => 'Client not found'], 404);
-        }
-
-        $user = $client->user;
-
-        return response()->json([
-            'data'    => $user->load('client')
-        ], 200);
+        return response()->json($client->load('user')->toArray(), 200);
     }
 
     public function store(StoreClientRequest $request)
     {
-        $data = $request->validated();
-        $userType = UserType::where('role', $data['role'])->firstOrFail();
+        $client = $this->clientService->createClient($request->validated());
 
-        $user = User::create([
-            'name'         => $data['name'],
-            'email'        => $data['email'],
-            'password'     => $data['password'],
-            'user_type_id' => $userType->id,
-        ]);
-
-        $user->client()->create([
-            'phone' => $data['phone'],
-            'address' => $data['address'],
-            'city' => $data['city'],
-        ]);
-
-        return response()->json([
-            'message' => $data['role'].' user created successfully',
-            'data' => $user->load('client')
-        ], 201);
+        return response()->json($client->load('user')->toArray(), 201);
     }
 
     public function update(UpdateClientRequest $request, $clientId)
     {
-        $client = Client::find($clientId);
+        $client = $this->clientService->updateClient($request->validated(), $clientId);
 
-        if(!$client){
-            return response()->json(['message' => 'Client not found'], 404);
-        }
-
-        $userData = collect($request->validated())->only(['name','email','password'])->toArray();
-
-        if(!empty($userData)){
-            $user = $client->user;
-            $user->update($userData);
-        }
-
-        $clientData = collect($request->validated())->only(['phone','address','city'])->toArray();
-
-        if(!empty($clientData)){
-            $client->update($clientData);
-        }
-
-        return response()->json([
-            'data' => $client->load('user')
-        ], 200);
+        return response()->json($client->load('user')->toArray(), 200);
     }
 
     public function destroy(string $id)
     {
-        $client = Client::find($id);
-
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
-        }
-
-        $client->user()->delete();
-
-        $client->delete();
+        $this->clientService->deleteClient($id);
 
         return response()->json([
             'message' => 'Client deleted successfully'
