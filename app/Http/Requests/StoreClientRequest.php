576258b986a96fb6b3a458a2dbf6fed22f9164ad
Christian Mota <christiancorreiamota@gmail.com>
2025-08-17 13:52:26 -0300

    feat(request): create StoreClientRequest

---
+++ app/Http/Requests/StoreClientRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreClientRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'     => 'required|string|min:5|max:255',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+            'role' => 'required|string|in:Admin,Client',
+            'phone' => 'required|string|min:9|max:11',
+            'address' => 'required|string|min:5|max:255',
+            'city' => 'required|string|min:3|max:255'
+        ];
+    }
+}
