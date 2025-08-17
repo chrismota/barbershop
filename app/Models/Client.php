9c01344b5381ce9653efc8fdf63fced807528b37
Christian Mota <christiancorreiamota@gmail.com>
2025-08-17 14:21:47 -0300

    feat(models): add softDeletes property in Client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model {
+    use SoftDeletes;
+
     public $incrementing = false;
     protected $keyType = 'string';
 
