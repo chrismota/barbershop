d9a4080b96b4a751ea191ebeae37e3bfa97586cb
Christian Mota <christiancorreiamota@gmail.com>
2025-08-18 11:59:01 -0300

    feat(routes): create scheduling routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\SchedulingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::apiResource('/users', UserController::class);
+Route::get('/clients/schedulings', [SchedulingController::class, 'index']);
 Route::apiResource('/clients', ClientController::class);
+Route::post('/clients/schedulings/{clientId}', [SchedulingController::class, 'store']);
+Route::delete('/clients/{clientId}/schedulings/{schedulingId}', [SchedulingController::class, 'destroy']);
+
+Route::get('/scheduling/available-slots', [SchedulingController::class, 'getAvailableSlots']);
+
+
