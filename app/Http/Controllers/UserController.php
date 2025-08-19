6f1b769962fafe085ff1fa0ae8c227cefe88718f
Christian Mota <christiancorreiamota@gmail.com>
2025-08-19 17:29:41 -0300

    feat(controller): create new CRUD methods for logged user in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -15,24 +16,42 @@ class UserController extends Controller
         $this->userService = $userService;
     }
 
-    public function index(){
+    public function index()
+    {
         return response()->json($this->userService->getAllUsers(), 200);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $user = $this->userService->getUser($id);
 
         return response()->json($user->toArray(), 200);
     }
 
-    public function store(StoreUserRequest $request) {
+    public function showLoggedUser()
+    {
+        $user = $this->userService->getUser(Auth::id());
+
+        return response()->json($user->toArray(), 200);
+    }
+
+    public function store(StoreUserRequest $request)
+    {
         $user = $this->userService->createUser($request->validated());
 
         return response()->json($user->toArray(), 201);
     }
 
-    public function update(UpdateUserRequest $request, $userId) {
+    public function update(UpdateUserRequest $request, $userId)
+    {
         $user = $this->userService->updateUser($request->validated(), $userId);
+
+        return response()->json($user->toArray(), 200);
+    }
+
+    public function updateLoggedUser(UpdateUserRequest $request)
+    {
+        $user = $this->userService->updateUser($request->validated(), Auth::id());
 
         return response()->json($user->toArray(), 200);
     }
@@ -46,4 +65,12 @@ class UserController extends Controller
         ], 204);
     }
 
+    public function destroyLoggedUser()
+    {
+        $this->userService->deleteUser(Auth::id());
+
+        return response()->json([
+            'message' => 'Admin deleted successfully',
+        ], 204);
+    }
 }
