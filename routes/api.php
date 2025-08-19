1cda99bf66f0bb77376eef82e9de57c68671c51b
Christian Mota <christiancorreiamota@gmail.com>
2025-08-18 22:39:12 -0300

    feat(routes): add get and update routes for scheduling

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\UserController;
 Route::apiResource('/users', UserController::class);
 Route::get('/clients/schedulings', [SchedulingController::class, 'index']);
 Route::apiResource('/clients', ClientController::class);
+Route::get('/clients/schedulings/{schedulingId}', [SchedulingController::class, 'show']);
 Route::post('/clients/schedulings/{clientId}', [SchedulingController::class, 'store']);
+Route::put('/clients/{clientId}/schedulings/{schedulingId}', [SchedulingController::class, 'update']);
 Route::delete('/clients/{clientId}/schedulings/{schedulingId}', [SchedulingController::class, 'destroy']);
 
 Route::get('/scheduling/available-slots', [SchedulingController::class, 'getAvailableSlots']);
