fb55fc3087616785c0246b37b2102e61e017da6b
Christian Mota <christiancorreiamota@gmail.com>
2025-08-20 13:15:22 -0300

    fix(request): adjust ClientRequest validation to correctly get user_id

--- app/Http/Requests/UpdateClientRequest.php
+++ app/Http/Requests/UpdateClientRequest.php
@@ -15,8 +15,15 @@ class UpdateClientRequest extends FormRequest
 
     public function rules(): array
     {
-        $clientId = $this->route('clientId') ?? Auth::id();
-        $userId   = Client::find($clientId)?->user_id;
+        $clientId = $this->route('clientId');
+
+        if ($clientId) {
+            $userId = Client::find($clientId)?->user_id;
+        } else {
+            $userId = Auth::id();
+            $client = Client::where('user_id', $userId)->first();
+            $clientId = $client?->id;
+        }
 
         return [
             'name'     => 'sometimes|string|min:5|max:255',
